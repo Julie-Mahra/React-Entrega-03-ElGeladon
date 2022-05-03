837b761b59d461c0eeb7702af02ae1365113c691
Juliana Mara <julianamara204@gmail.com>
2022-05-02 21:20:31 -0300

    Entrega-03

---
+++ src/mocks/paletas.js
@@ -0,0 +1,181 @@
+export const paletas = [
+    {
+        titulo: "Açaí com Leite Condensado",
+        descricao:
+            "Paleta feita com o delicioso fruto tropical (Açaí), super nutritivo, com recheio de Leite condensado.",
+        foto: require("../assets/images/acai-com-leite-condensado.png"),
+        preco: 10.0,
+        sabor: "Açaí",
+        recheio: "Leite Condensado",
+        possuiRecheio: true,
+    },
+    {
+        titulo: "Banana com Nutella",
+        descricao:
+            "Paleta feita com Banana e recheio cremoso de Nutella.",
+        foto: require("../assets/images/banana-com-nutella.png"),
+        preco: 10.0,
+        sabor: "Banana",
+        recheio: "Nutella",
+        possuiRecheio: true,
+    },
+    {
+        titulo: "Chocolate Belga",
+        descricao:
+            "Paleta feita com Chocolate Belga à 90% chocolate e 10% creme de leite.",
+        foto: require("../assets/images/chocolate-belga.png"),
+        preco: 7.0,
+        sabor: "Chocolate Belga",
+        possuiRecheio: false,
+    },
+    {
+        titulo: "Chocolate Belga com Brigadeiro",
+        descricao:
+            "Paleta feita com Chocolate Belga à 90% e recheio de Brigadeiro.",
+        foto: require("../assets/images/chocolate-belga-com-brigadeiro.png"),
+        preco: 10.0,
+        sabor: "Chocolate Belga",
+        recheio: "Brigadeiro",
+        possuiRecheio: true,
+    },
+    {
+        titulo: "Chocolate Branco",
+        descricao:
+            "Paleta feita com Chocolate Branco.",
+        foto: require("../assets/images/chocolate-branco.png"),
+        preco: 7.0,
+        sabor: "Chocolate Branco",
+        possuiRecheio: false,
+    },
+    {
+        titulo: "Côco",
+        descricao:
+            "Paleta feita com Côco.",
+        foto: require("../assets/images/coco.png"),
+        preco: 7.0,
+        sabor: "Côco",
+        possuiRecheio: false,
+    },
+    {
+        titulo: "Côco com Doce de Leite",
+        descricao:
+            "Paleta feita com Côco e recheio de Doce de Leite de Viçosa/MG (o melhor Doce de Leite do Mundo!)",
+        foto: require("../assets/images/coco-com-doce-de-leite.png"),
+        preco: 10.0,
+        sabor: "Côco",
+        recheio: "Doce de Leite",
+        possuiRecheio: true,
+    },
+    {
+        titulo: "Cookies",
+        descricao:
+            "Paleta feita com Cookies crocantes num delicioso creme.",
+        foto: require("../assets/images/cookies.png"),
+        sabor: "Cookies",
+        possuiRecheio: false,
+    },
+    {
+        titulo: "Doce de Leite",
+        descricao:
+            "Doce de Leite com Doce de Leite de Viçosa/MG e recheio cremoso de Doce de Leite de Viçosa/MG.",
+        foto: require("../assets/images/doce-de-leite-com-doce-de-leite.png"),
+        preco: 10.0,
+        sabor: "Doce de Leite",
+        recheio: "Doce de Leite",
+        possuiRecheio: true,
+    },
+    {
+        titulo: "Mousse de Limão",
+        descricao:
+            "Paleta feita com Mousse de Limão, super cremosa.",
+        foto: require("../assets/images/mousse-de-limao.png"),
+        preco: 7.0,
+        sabor: "Mousse de Limão",
+        possuiRecheio: false,
+    },
+    {
+        titulo: "Maracujá",
+        descricao:
+            "Paleta feita com Mousse de Maracujá, super cremosa.",
+        foto: require("../assets/images/maracuja.png"),
+        sabor: "Maracujá",
+        possuiRecheio: false,
+    },
+    {
+        titulo: "Maracujá com Leite Condensado",
+        descricao:
+            "Paleta feita com Mousse de Maracujá, e recheio de Leite Condensado, super cremoso.",
+        foto: require("../assets/images/maracuja-com-leite-condensado.png"),
+        preco: 10.0,
+        sabor: "Maracujá",
+        recheio: "Leite Condensado",
+        possuiRecheio: true,
+    },
+    {
+        titulo: "Milho Verde",
+        descricao:
+            "Paleta feita com Creme de Milho Verde.",
+        foto: require("../assets/images/milho-verde.png"),
+        preco: 7.0,
+        sabor: "Milho Verde",
+        possuiRecheio: false,
+    },
+    {
+        titulo: "Morango",
+        descricao:
+            "Paleta vegana feita com Morangos, sem adição de corantes/conservantes.",
+        foto: require("../assets/images/morango.png"),
+        preco: 7.0,
+        sabor: "Morango",
+        possuiRecheio: false,
+    },
+    {
+        titulo: "Morango com Leite Condensado",
+        descricao:
+            "Paleta feita com Morangos, e recheio de Leite Condensado, super cremoso.",
+        foto: require("../assets/images/morango-com-leite-condensado.png"),
+        preco: 10.0,
+        sabor: "Morango",
+        recheio: "Leite Condensado",
+        possuiRecheio: true,
+    },
+    {
+        titulo: "Morango com Nutella",
+        descricao:
+            "Paleta feita com Morangos, e recheio de creme de Nutella.",
+        foto: require("../assets/images/morango-com-nutella.png"),
+        preco: 10.0,
+        sabor: "Morango",
+        recheio: "Nutella",
+        possuiRecheio: true,
+    },
+    {
+        titulo: "Ninho com Nutella",
+        descricao:
+            "Paleta feita com Leite Ninho e recheio de Creme de Nutella.",
+        foto: require("../assets/images/ninho-com-nutella.png"),
+        preco: 10.0,
+        sabor: "Ninho",
+        recheio: "Nutella",
+        possuiRecheio: true,
+    },
+    {
+        titulo: "Ninho com Ovomaltine",
+        descricao:
+            "Paleta feita com Leite Ninho e recheio crocante de Ovomaltine.",
+        foto: require("../assets/images/ninho-com-ovomaltine.png"),
+        preco: 10.0,
+        sabor: "Ninho",
+        recheio: "Ovomaltine",
+        possuiRecheio: true,
+    },
+    {
+        titulo: "Uva",
+        descricao:
+            "Paleta feita com Uvas.",
+        foto: require("../assets/images/uva.png"),
+        preco: 7.0,
+        sabor: "Uva",
+        possuiRecheio: false,
+    },
+];
